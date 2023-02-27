325babbf7f2684676b9d982eb6faa7524bc329d9
HongkyuRyu <minsubrother@naver.com>
2023-02-27 12:24:14 +0900

    my_first_porfolio

---
+++ my-react-app/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import profileImg from "../../assets/profile-img.png";
+import shapeOne from "../../assets/shape-1.png";
+import shapeTwo from "../../assets/shape-2.png";
+
+import { BsGithub, FaPencilAlt, AiFillInstagram } from "react-icons/Bs";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
